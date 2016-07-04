cfc7675d7335ccfd866cfe5bd99f8ab51d8d423b
luqmament <alakazamta.warning@gmail.com>
2016-07-04 13:16:11 +0700

    edit

--- application/views/error_pageCustom.php
+++ application/views/error_pageCustom.php
@@ -9,7 +9,7 @@
                                <img src="<?php echo base_url();?>assets/rewards/images/plank-1.png">
                                 <p style="position:absolute; margin:-400px 0 0 180px;font-size:26px;text-align:center; color:#002244;font-weight:bold;">
                                     <span style="font-size:48px;">Maaf...</span><br>
-                                    Halaman yang kamu cari tidak ditemukan
+                                    Halaman yang kamu cari tidak ditemukan yaaa
                                 </p>
                                 <div style="position:absolute;margin:-260px 0 0 180px;">
                                  <a href="<?php echo base_url()?>">
